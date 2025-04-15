6a90bcdfbff87b2b003cc413fb6ec6c2da4a7ee6
jagadwp <jagadpurnomo26@gmail.com>
2025-04-15 23:04:36 +0700

    fix: typo navbar

--- src/layout/topNavBar/TopNavBar.vue
+++ src/layout/topNavBar/TopNavBar.vue
@@ -11,7 +11,7 @@ const pokemonItem = usePokemonItemStore()
       bottom color="deep-purple-accent-4"></v-progress-linear>
     <template v-slot:append>
       <h3 class="mr-5 ml-1">
-        Poxedex by Jagad 
+        Pokedex by Jagad 
       </h3>
       <ThemeButton class="d-flex align-center mr-4" />
       <OptionButton />
